703b6f0233f6104c8a66ebca55d43ff9d4fc40fb
Lun Tan <tanlun@amazon.com>
2022-05-08 21:28:01 -0400

    add more oas

--- src/main/java/tiktok/oa3/Solution.java
+++ src/main/java/tiktok/oa3/Solution.java
@@ -1,4 +1,45 @@
 package tiktok.oa3;
 
+import java.util.*;
+//import java.util.List;
+
 public class Solution {
+    protected static final boolean SOLUTION_DONE = true;
+
+    public int getMaxDistance(HashMap<Integer, List<Integer>> graph) {
+
+        HashSet<Integer> aSet = new HashSet<>();
+        aSet.add(1);
+        aSet.add(2);
+        aSet.add(3);
+
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+
+        HashSet<Integer> visited = new HashSet<>();
+        visited.add(4);
+        visited.add(5);
+        visited.add(6);
+
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            level += 1;
+            for (int i = 0; i < size; i++) {
+                int number = queue.pollFirst();
+                System.out.println(number);
+                for (int connectedNumber : graph.get(number)) {
+                    if (!visited.contains(connectedNumber)) {
+                        visited.add(connectedNumber);
+                        queue.add(connectedNumber);
+                    }
+                }
+            }
+
+        }
+
+        return level - 1;
+    }
 }
--- src/test/java/tiktok/oa3/SolutionTest.java
+++ src/test/java/tiktok/oa3/SolutionTest.java
@@ -1,4 +1,58 @@
 package tiktok.oa3;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 public class SolutionTest {
+    tiktok.oa3.Solution solution;
+
+    @BeforeMethod
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @Test(enabled = Solution.SOLUTION_DONE)
+    public void testNumberInfected () throws Exception {
+
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(2);
+        list1.add(3);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(3);
+        list2.add(4);
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(1);
+        list3.add(2);
+        list3.add(5);
+        List<Integer> list4 = new ArrayList<>();
+        list4.add(2);
+        list4.add(5);
+        list4.add(6);
+        List<Integer> list5 = new ArrayList<>();
+        list5.add(1);
+        list5.add(2);
+        list5.add(3);
+        list5.add(4);
+        list5.add(6);
+        List<Integer> list6 = new ArrayList<>();
+        list6.add(4);
+        list6.add(5);
+        graph.put(1, list1);
+        graph.put(2, list2);
+        graph.put(3, list3);
+        graph.put(4, list4);
+        graph.put(5, list5);
+        graph.put(6, list6);
+        assertEquals(solution.getMaxDistance(graph), 1);
+
+
+    }
 }
